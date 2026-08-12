797215915a1b40385e3afb664ee83e314d350d9b
mohammadrimanieh-ship-it <mohammad.r.imanieh@gmail.com>
2026-08-12 23:37:44 +0330

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
 plugins {
-    id("com.android.application") version "9.3.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.3.21" apply false
-    id("org.jetbrains.kotlin.kapt") version "2.3.21" apply false
+    id("com.android.application") version "9.0.0" apply false
 }
